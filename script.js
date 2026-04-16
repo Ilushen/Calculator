a9079203de38ada52e71b2048f8accaab3e50b4b
Ilushen Banze <58256890+Ilushen@users.noreply.github.com.>
2026-04-16 14:46:32 +0200

    build the calculator logic the calculator now allow the user to clik the buttons and display the number corresponding to the buttons

--- script.js
+++ script.js
@@ -22,7 +22,19 @@ function divide (a,b){
     return a/b;
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+const display = document.querySelector("#display");
+const btn = document.querySelectorAll("button");
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.value = button.textContent;
+    
+    if(firstNumber == "")
+        firstNumber = display.value;
+    else if (operator == "")
+        operator = display.value;
+    else if(secondNumber == "")
+        secondNumber = display.value
+    else
+        operate(operator,firstNumber,secondNumber);
+  });
+});
